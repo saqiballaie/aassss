a6d5fc91e2ae86446215aa8cd2f21520d81e9eb5
saqiballaie <saqib.parvaze@gmail.com>
2017-01-22 18:55:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+*WEATHER UPDATE* _District Wise_<br>
+<?php
+include "banihal.php";
+include "batote.php";
+include "bhaderwah.php";
+include "gulmarg.php";
+include "jammu.php";
+include "katra.php";
+include "kukernag.php";
+include "kupwara.php";
+include "leh.php";
+include "pahalgam.php";
+include "qazigund.php";
+include "srinagar.php";
+?>
+<br>
+++++++++++++<br>
+Get this update directly in your *WhatsApp* inbox. <br>
+Text _Weather Your Name_ to _+919797248710_ on WhatsApp.<br>
+++++++++++++<br>
+Help us reach everyone. Share this update with everyone.
---
+++ katra.php
@@ -0,0 +1,29 @@
+<?php 
+$date= date("d/m/y");
+$date1= date("d");
+$file=file_get_contents("http://14.139.247.11/citywx/city_weather.php?id=42054");
+$file=str_replace('<center>','',$file);
+$file=str_replace('Past 24 Hours Weather Data','',$file);
+$file=str_replace('img/katra.gif','',$file);
+$file=str_replace('Local Weather Report and Forecast For:','',$file);
+$file=str_replace('tr','br',$file);
+$file=str_replace("Kabra","Katra",$file);
+$file=str_replace('<FONT color=blue>Katra</Font>','============<br>*Katra*',$file);
+$file=str_replace('<B>Dated :','Date: _',$file);
+$file=str_replace('2017</b>','2017_<br>============',$file);
+$file=str_replace('<table','<span',$file);
+$file=str_replace('</tr>','',$file);
+$file=str_replace('</br>','',$file);
+$file=str_replace('<img','<imgs',$file);
+$file=str_replace('<IMG','<IMGS',$file);
+$file=str_replace('font','fonts',$file);
+$file=str_replace('<br><td','<td',$file);
+$file=str_replace("7 Day's","============<br>7 Day's",$file);
+$file=str_replace('<a href="http://www.imd.gov.in/section/climate/exbreme/kabra2.htm">Exbreme & Climatological Information</a>','',$file);
+$file=str_replace('( )','',$file);
+$file=str_replace('Recorded. on ','',$file);
+$file=str_replace('(Recorded from 0830 hrs IST <BR>of yesterday to 0830 hrs IST of today)','',$file);
+$file=str_replace('with possibility of','or',$file);
+echo $file;
+echo "</b>";
+?>
